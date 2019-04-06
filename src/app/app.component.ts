00a9d1858dfe6d5a26d21b2ae219c40a82eca281
Droni <mrdroonix@gmail.com>
2019-04-06 23:19:25 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h3 {
+      color: deeppink;
+    }
+  `]
+})
+export class AppComponent {
+  title = 'AngularApp';
+}
